c1ca2cadba3120ada82a2a347c0945a3eab08c12
michael1yu <31612776+michael1yu@users.noreply.github.com>
2023-03-20 12:49:32 -0700

    Update init_vars.py

--- scripts/init_vars.py
+++ scripts/init_vars.py
@@ -50,25 +50,11 @@ precursors = ['weizmann',
               'utkinect',
               'utdmhad']
 
-archives_paths = dict(kth=archives_path + 'kth/',
-                      ixmas=archives_path + 'ixmas/',
-                      i3dpost=archives_path + 'i3dpost/',
-                      weizmann=archives_path + 'weizmann/',
-                      isld=archives_path + 'isld/',
-                      isldas=archives_path + 'isldas/',
-                      utkinect=archives_path + 'utkinect/',
-                      utdmhad=archives_path + 'utdmhad/',
+archives_paths = dict(utkinect=archives_path + 'utkinect/',
                       json=archives_path + 'json/',
                       posenet=archives_path + 'posenet/')
 
-former_paths = dict(kth=temp_path + 'kth/',
-                    ixmas=temp_path + 'ixmas/',
-                    i3dpost=temp_path + 'i3dpost/',
-                    weizmann=temp_path + 'weizmann/',
-                    isld=temp_path + 'isld/',
-                    isldas=temp_path + 'isldas/',
-                    utkinect=temp_path + 'utkinect/',
-                    utdmhad=temp_path + 'utdmhad/')
+former_paths = dict(utkinect=temp_path + 'utkinect/')
 
 paths = dict(video=data_path + 'video/',
              pose=data_path + 'openpose/',
@@ -98,40 +84,14 @@ actions = {'standing': 0,
            'pjump': 18,
            'run': 19}
 
-fps = dict(kth=25,
-           ixmas=25,
-           i3dpost=25,
-           weizmann=25,
-           isld=25,
-           isldas=19,
-           utkinect=19,
-           utdmhad=15)
+fps = dict(utkinect=19)
 
-former_frame_size = dict(kth=(160, 120),
-                         ixmas=(64, 48),
-                         i3dpost=(1920, 1080),
-                         weizmann=(180, 144),
-                         isld=(1920, 1080),
-                         isldas=(960, 600),
-                         utkinect=(640, 480),
-                         utdmhad=(640, 480))
+former_frame_size = dict(utkinect=(640, 480))
 
-frame_size = dict(kth=(160, 120),
-                  ixmas=(48, 64),
-                  i3dpost=(480, 270),
-                  weizmann=(180, 144),
-                  isld=(480, 270),
-                  isldas=(960, 600),
-                  utkinect=(640, 480),
-                  utdmhad=(640, 480))
+frame_size = dict(utkinect=(640, 480)))
 resize_interpolation = cv2.INTER_CUBIC
 
-misc_paths = dict(kth=dataset_path+'scripts/misc/kth_splitter.csv',
-                  ixmas=dataset_path+'scripts/misc/ixmas_exclude.csv',
-                  i3dpost=dataset_path+'scripts/misc/i3dpost_archives.csv',
-                  isld=dataset_path+'scripts/misc/isld_truth.xlsx',
-                  utkinect=dataset_path+'scripts/misc/utkinect_splitter.txt',
-                  utdmhad=dataset_path+'scripts/misc/utdmhad_truth.csv',
+misc_paths = dict(utkinect=dataset_path+'scripts/misc/utkinect_splitter.txt',
                   checksum_video=dataset_path+'scripts/misc/cksum_video.csv',
                   checksum_rgb=dataset_path+'scripts/misc/cksum_rgb.csv',
                   checksum_pose=dataset_path+'scripts/misc/cksum_pose.csv',
